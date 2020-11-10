033cf72bf3c6d1caa2be63b63feba55cafedde4f
kerstin <kerstinpawellek@hotmail.com>
2020-11-10 21:10:31 +0100

    Add Monte-Carlo pi computation

--- main.cpp
+++ main.cpp
@@ -9,7 +9,19 @@ int main() {
   uniform_real_distribution<float> dist{0, 1};
   auto random = [&]() { return dist(rng); };
 
-  for (int i = 0; i < 10; ++i)
-    // cout << dist(rng) << '\n';
-    cout << random() << '\n';
+  int n = 100;
+  int c = 0;
+
+  for (int i = 0; i < 10; ++i) {
+    const auto x = random();
+    const auto y = random();
+    const auto r2 = x * y + y * y;
+    if (r2 < 1) ++c;
+  }
+
+  auto monte_carlo_pi = 4 * float(c) / float(n);
+  cout << "pi=" << monte_carlo_pi << '\n';
+
+  // cout << dist(rng) << '\n';
+  // cout << random() << '\n';
 }
